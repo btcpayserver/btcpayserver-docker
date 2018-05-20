c9a4f2e5d5502aae8a69bcea70a2781bce625a42
nicolas.dorier <nicolas.dorier@gmail.com>
2018-05-20 17:16:42 +0900

    bump

--- build-pregen.ps1
+++ build-pregen.ps1
@@ -1,4 +1,7 @@
 # This script will run docker-compose-generator in a container to generate the yml files
 
 docker pull btcpayserver/docker-compose-generator
-docker run -v "$(Get-Location)\Production:/app/Production" -v "$(Get-Location)\Production-NoReverseProxy:/app/Production-NoReverseProxy" --rm btcpayserver/docker-compose-generator pregen
+docker run -v "$(Get-Location)\Production:/app/Production" `
+           -v "$(Get-Location)\Production-NoReverseProxy:/app/Production-NoReverseProxy" `
+           -v "$(Get-Location)\docker-compose-generator\docker-fragments:/app/docker-fragments" `
+           --rm btcpayserver/docker-compose-generator pregen
