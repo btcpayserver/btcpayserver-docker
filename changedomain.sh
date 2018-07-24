8b8f7075bad54a8a26e0f0f992875a897f8642ba
nicolas.dorier <nicolas.dorier@gmail.com>
2018-07-24 23:24:10 +0900

    remove useless sleep

--- changedomain.sh
+++ changedomain.sh
@@ -19,7 +19,6 @@ sed -i '/^ACME_CA_URI/d' "$BTCPAY_ENV_FILE"
 echo "BTCPAY_HOST=$BTCPAY_HOST" >> "$BTCPAY_ENV_FILE"
 echo "ACME_CA_URI=$ACME_CA_URI" >> "$BTCPAY_ENV_FILE"
 
-sleep 1
 cd "`dirname $BTCPAY_ENV_FILE`"
 docker-compose -f "$BTCPAY_DOCKER_COMPOSE" up -d
 fi
