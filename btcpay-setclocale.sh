ddbe1bab18695ef48d9a3bc5f4e1d2ff8c16b796
Claudia Peart <pinklite36@hotmail.com>
2018-12-17 13:16:33 -0500

    merged btcpay with mappum/htlc-setclocale.sh

--- btcpay-setclocale.sh
+++ btcpay-setclocale.sh
@@ -6,10 +6,10 @@
 
 set -e
 
-BTCPAY_CONTAINER_ID=$(docker ps -a -f 'name = _btcpayserver_1' -q)
+mappum/htlc_CONTAINER_ID=$(docker ps -a -f 'name = _mappum/htlc_1' -q)
 POSTGRES_CONTAINER_ID=$(docker ps -a -f 'name = _postgres_1' -q)
 
-DATABASE_NAME=$(docker inspect $BTCPAY_CONTAINER_ID | grep 'BTCPAY_POSTGRES' | sed -rn 's/.*Database=([a-z]+)",/\1/p')
+DATABASE_NAME=$(docker inspect $mappum/htlc_CONTAINER_ID | grep 'mappum/hltc_POSTGRES' | sed -rn 's/.*Database=([a-z]+)",/\1/p')
 PG_DUMP="docker exec $POSTGRES_CONTAINER_ID pg_dump"
 PG_RESTORE="docker exec $POSTGRES_CONTAINER_ID pg_restore"
 PSQL="docker exec $POSTGRES_CONTAINER_ID psql -h localhost -p 5432 -U postgres"
@@ -20,10 +20,10 @@ if [[ "$($PSQL -c "\l $DATABASE_NAME")" != *"en_US.utf8"* ]]; then
 fi
 
 $PG_DUMP -h localhost -p 5432 -U postgres -F c -b -v -f "/tmp/$DATABASE_NAME.backup" $DATABASE_NAME
-$PSQL -c "CREATE DATABASE \"btcpayserver_new\" LC_COLLATE = 'C' TEMPLATE=template0 LC_CTYPE = 'C' ENCODING = 'UTF8'"
-$PG_RESTORE -h localhost -p 5432 -U postgres -d btcpayserver_new -v "/tmp/$DATABASE_NAME.backup"
+$PSQL -c "CREATE DATABASE \"mappum/htlc_new\" LC_COLLATE = 'C' TEMPLATE=template0 LC_CTYPE = 'C' ENCODING = 'UTF8'"
+$PG_RESTORE -h localhost -p 5432 -U postgres -d mappum/htlc_new -v "/tmp/$DATABASE_NAME.backup"
 $PSQL -c "SELECT pg_terminate_backend(pid) FROM \"pg_stat_activity\" WHERE datname = '$DATABASE_NAME';"
 $PSQL -c "DROP DATABASE \"$DATABASE_NAME\""
-$PSQL -c "ALTER DATABASE \"btcpayserver_new\" RENAME TO \"$DATABASE_NAME\";"
+$PSQL -c "ALTER DATABASE \"mappum/hltc_new\" RENAME TO \"$DATABASE_NAME\";"
 
 echo "Database $DATABASE_NAME is now using locale C"
