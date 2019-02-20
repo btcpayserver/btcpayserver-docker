4732646cd84ab0f7f002a4ab2ca179130200211d
nicolas.dorier <nicolas.dorier@gmail.com>
2019-02-21 00:01:52 +0900

    fix btcpay-down for some installs

--- btcpay-down.sh
+++ btcpay-down.sh
@@ -3,4 +3,6 @@
 . /etc/profile.d/btcpay-env.sh
 
 cd "`dirname $BTCPAY_ENV_FILE`"
-docker-compose -f $BTCPAY_DOCKER_COMPOSE down -t "${COMPOSE_HTTP_TIMEOUT:-180}"
+if ! docker-compose -f $BTCPAY_DOCKER_COMPOSE down -t "${COMPOSE_HTTP_TIMEOUT:-180}" 2>/dev/null; then
+    docker-compose -f $BTCPAY_DOCKER_COMPOSE down
+fi
