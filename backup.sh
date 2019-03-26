045aa8d92b2dd980942d67a224ed6dc4144a3e65
Jeff Vandrew Jr <jeffvandrew@yahoo.com>
2019-03-26 02:19:20 -0400

    dockerize script

--- backup.sh
+++ backup.sh
@@ -18,9 +18,15 @@ if [ -z ${BACKUP_PROVIDER+x} ]; then
     echo "Set BACKUP_PROVIDER environmental variable and try again."
     exit 1
 elif [ ${BACKUP_PROVIDER="Dropbox"} ]; then
+    if [ -z ${DROPBOX_TOKEN+x} ]; then
+        echo "Set DROPBOX_TOKEN environmental variable and try again."
+        exit 1
+    fi
+    if [ ! -d /var/lib/docker/volumes/backup_datadir ]; then
+        docker volume create backup_datadir
+    fi	
     btcpay-down.sh
-    tar -cvzf $PWD/dropbox-script/backup.tar.gz --exclude='/var/lib/docker/volumes/generated_bitcoin_datadir/*' /var/lib/docker
+    tar --exclude='/var/lib/docker/volumes/backup_datadir/*' --exclude='/var/lib/docker/volumes/generated_bitcoin_datadir/*' -cvzf /var/lib/docker/volumes/backup_datadir/_data/backup.tar.gz /var/lib/docker
     btcpay-up.sh
-    cd dropbox-script
-    ./dropbox-script && rm backup.tar.gz
+    docker run -v backup_datadir:/data --env DROPBOX_TOKEN=$DROPBOX_TOKEN jvandrew/btcpay-dropbox:1.0.1
 fi
--- dropbox-script/dropbox/__init__.py
+++
@@ -1,4 +0,0 @@
-from __future__ import absolute_import
-
-from .dropbox import __version__, Dropbox, DropboxTeam, create_session  # noqa: F401
-from .oauth import DropboxOAuth2Flow, DropboxOAuth2FlowNoRedirect  # noqa: F401
