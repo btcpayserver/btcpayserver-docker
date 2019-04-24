5085f187c3c9dd502a36e04b6c147570ebba2c5f
nicolas.dorier <nicolas.dorier@gmail.com>
2019-04-24 14:27:28 +0900

    in btcpay-down, check result of docker-compose down

--- btcpay-down.sh
+++ btcpay-down.sh
@@ -3,6 +3,8 @@
 . /etc/profile.d/btcpay-env.sh
 
 cd "`dirname $BTCPAY_ENV_FILE`"
-if ! docker-compose -f $BTCPAY_DOCKER_COMPOSE down -t "${COMPOSE_HTTP_TIMEOUT:-180}" 2>/dev/null; then
+
+docker-compose -f $BTCPAY_DOCKER_COMPOSE down -t "${COMPOSE_HTTP_TIMEOUT:-180}"
+if ! [ $? -eq 0 ] then
     docker-compose -f $BTCPAY_DOCKER_COMPOSE down
 fi
