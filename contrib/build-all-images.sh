10864d842055941f20688f23a00f9af342a1de46
nicolas.dorier <nicolas.dorier@gmail.com>
2019-06-04 10:30:29 +0900

    bump

--- contrib/build-all-images.sh
+++ contrib/build-all-images.sh
@@ -165,16 +165,16 @@ cd - && cd ..
 
 
 # Build btcpayserver
-# https://raw.githubusercontent.com/btcpayserver/btcpayserver/v1.0.3.112/amd64.Dockerfile
+# https://raw.githubusercontent.com/btcpayserver/btcpayserver/v1.0.3.113/amd64.Dockerfile
 DOCKERFILE="amd64.Dockerfile"
-# https://raw.githubusercontent.com/btcpayserver/btcpayserver/v1.0.3.112/arm32v7.Dockerfile
+# https://raw.githubusercontent.com/btcpayserver/btcpayserver/v1.0.3.113/arm32v7.Dockerfile
 [[ "$(uname -m)" == "armv7l" ]] && DOCKERFILE="arm32v7.Dockerfile"
-echo "Building btcpayserver/btcpayserver:1.0.3.112"
+echo "Building btcpayserver/btcpayserver:1.0.3.113"
 git clone https://github.com/btcpayserver/btcpayserver btcpayserver
 cd btcpayserver
-git checkout v1.0.3.112
+git checkout v1.0.3.113
 cd "$(dirname $DOCKERFILE)"
-docker build -f "$DOCKERFILE" -t "btcpayserver/btcpayserver:1.0.3.112" .
+docker build -f "$DOCKERFILE" -t "btcpayserver/btcpayserver:1.0.3.113" .
 cd - && cd ..
 
 
