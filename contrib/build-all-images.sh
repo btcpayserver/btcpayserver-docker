caacaa9f6f74ba4a6036654a8266e038139675a4
Andrew Camilleri <evilkukka@gmail.com>
2019-06-11 08:13:11 +0200

    bump transmuter to .25 (#146)
    * bump transmuter to .25
    
    * bump to 26

--- contrib/build-all-images.sh
+++ contrib/build-all-images.sh
@@ -403,16 +403,16 @@ cd - && cd ..
 
 
 # Build btctransmuter
-# https://raw.githubusercontent.com/btcpayserver/btctransmuter/v0.0.24/BtcTransmuter/Dockerfile.linuxamd64
+# https://raw.githubusercontent.com/btcpayserver/btctransmuter/v0.0.26/BtcTransmuter/Dockerfile.linuxamd64
 DOCKERFILE="BtcTransmuter/Dockerfile.linuxamd64"
-# https://raw.githubusercontent.com/btcpayserver/btctransmuter/v0.0.24/BtcTransmuter/Dockerfile.linuxarm32v7
+# https://raw.githubusercontent.com/btcpayserver/btctransmuter/v0.0.26/BtcTransmuter/Dockerfile.linuxarm32v7
 [[ "$(uname -m)" == "armv7l" ]] && DOCKERFILE="BtcTransmuter/Dockerfile.linuxarm32v7"
-echo "Building btcpayserver/btctransmuter:0.0.24"
+echo "Building btcpayserver/btctransmuter:0.0.26"
 git clone https://github.com/btcpayserver/btctransmuter btctransmuter
 cd btctransmuter
-git checkout v0.0.24
+git checkout v0.0.26
 cd "$(dirname $DOCKERFILE)"
-docker build -f "$DOCKERFILE" -t "btcpayserver/btctransmuter:0.0.24" .
+docker build -f "$DOCKERFILE" -t "btcpayserver/btctransmuter:0.0.26" .
 cd - && cd ..
 
 
