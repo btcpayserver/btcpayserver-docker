515d6274dc8dd8e26dd61c19cea6f235d04486da
Wouter Samaey <wouter.samaey@storefront.be>
2019-06-15 21:07:31 +0200

    Mac OS support

--- btcpay-update.sh
+++ btcpay-update.sh
@@ -2,7 +2,16 @@
 
 set -e
 
-. /etc/profile.d/btcpay-env.sh
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	# Mac OS
+	BASH_PROFILE_SCRIPT="$HOME/btcpay-env.sh"
+
+else
+	# Linux
+	BASH_PROFILE_SCRIPT="/etc/profile.d/btcpay-env.sh"
+fi
+
+. ${BASH_PROFILE_SCRIPT}
 
 if [ ! -z $BTCPAY_DOCKER_COMPOSE ] && [ ! -z $DOWNLOAD_ROOT ] && [ -z $BTCPAYGEN_OLD_PREGEN ]; then 
     echo "Your deployment is too old, you need to migrate by following instructions on this link https://github.com/btcpayserver/btcpayserver-docker/tree/master#i-deployed-before-btcpay-setupsh-existed-before-may-17-can-i-migrate-to-this-new-system"
@@ -23,11 +32,11 @@ if [[ "$1" != "--skip-git-pull" ]]; then
 fi
 
 if ! [ -f "/etc/docker/daemon.json" ]; then
-echo "{
+	echo "{
 \"log-driver\": \"json-file\",
 \"log-opts\": {\"max-size\": \"5m\", \"max-file\": \"3\"}
 }" > /etc/docker/daemon.json
-echo "Setting limited log files in /etc/docker/daemon.json"
+	echo "Setting limited log files in /etc/docker/daemon.json"
 fi
 
 . ./build.sh
@@ -35,8 +44,8 @@ if [ "$BTCPAYGEN_OLD_PREGEN" == "true" ]; then
     cp Generated/docker-compose.generated.yml $BTCPAY_DOCKER_COMPOSE
 fi
 
-if ! grep -Fxq "export COMPOSE_HTTP_TIMEOUT=\"180\"" "/etc/profile.d/btcpay-env.sh"; then
-    echo "export COMPOSE_HTTP_TIMEOUT=\"180\"" >> /etc/profile.d/btcpay-env.sh
+if ! grep -Fxq "export COMPOSE_HTTP_TIMEOUT=\"180\"" "$BASH_PROFILE_SCRIPT"; then
+    echo "export COMPOSE_HTTP_TIMEOUT=\"180\"" >> ${BASH_PROFILE_SCRIPT}
     export COMPOSE_HTTP_TIMEOUT=180
     echo "Adding COMPOSE_HTTP_TIMEOUT=180 in btcpay-env.sh"
 fi
