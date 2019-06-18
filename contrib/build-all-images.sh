717d5f9c604bcfe103931e5fb8128c9284cf7a5d
nicolas.dorier <nicolas.dorier@gmail.com>
2019-06-18 13:32:16 +0900

    bump nbxplorer

--- contrib/build-all-images.sh
+++ contrib/build-all-images.sh
@@ -321,16 +321,16 @@ cd - && cd ..
 
 
 # Build nbxplorer
-# https://raw.githubusercontent.com/dgarage/nbxplorer/v2.0.0.50/Dockerfile.linuxamd64
+# https://raw.githubusercontent.com/dgarage/nbxplorer/v2.0.0.51/Dockerfile.linuxamd64
 DOCKERFILE="Dockerfile.linuxamd64"
-# https://raw.githubusercontent.com/dgarage/nbxplorer/v2.0.0.50/Dockerfile.linuxarm32v7
+# https://raw.githubusercontent.com/dgarage/nbxplorer/v2.0.0.51/Dockerfile.linuxarm32v7
 [[ "$(uname -m)" == "armv7l" ]] && DOCKERFILE="Dockerfile.linuxarm32v7"
-echo "Building nicolasdorier/nbxplorer:2.0.0.50"
+echo "Building nicolasdorier/nbxplorer:2.0.0.51"
 git clone https://github.com/dgarage/nbxplorer nbxplorer
 cd nbxplorer
-git checkout v2.0.0.50
+git checkout v2.0.0.51
 cd "$(dirname $DOCKERFILE)"
-docker build -f "$DOCKERFILE" -t "nicolasdorier/nbxplorer:2.0.0.50" .
+docker build -f "$DOCKERFILE" -t "nicolasdorier/nbxplorer:2.0.0.51" .
 cd - && cd ..
 
 
