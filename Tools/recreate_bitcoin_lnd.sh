f608a237bdc95f0f295699f5a587964b5434886d
Rockstar Developer <5191402+rockstardev@users.noreply.github.com>
2019-11-16 04:27:37 -0600

    LND Seed Backup (#234)
    * Preparing for testing of LND Seed Backup
    
    * Adding variable with path to walletunlock file
    
    * Recreating Bitcoin LND container
    
    * Bumping btcpayserver reference to next version 1.0.3.138
    
    * Fixing text per suggestion

---
+++ Tools/recreate_bitcoin_lnd.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+read -p "This script will delete and recreate your LND Bitcoin container. YOU CAN'T UNDO THIS OPERATION, ALL FUNDS THAT YOU CURRENTLY HAVE ON THIS LND WILL BE LOST! Type 'yes' to proceed only after you've transfered all your funds from this LND instance `echo $'\n> '`" yn
+if [ $yn != "yes" ]; then
+	exit 0
+fi
+
+read -p "Only proceed if you've removed all the funds from LND Bitcoin container! This LND instance will be completely deleted and all data from it unrecoverable. Type 'yes' to proceed only if you are 100% sure `echo $'\n> '`" yn
+if [ $yn != "yes" ]; then
+	exit 0
+fi
+
+read -p "OK, last chance to abort. Type 'yes' to continue! `echo $'\n> '`" yn
+if [ $yn != "yes" ]; then
+	exit 0
+fi
+
+../btcpay-down.sh
+
+docker volume rm --force generated_lnd_bitcoin_datadir
+
+../btcpay-up.sh
+
+echo "LND container recreated"
