cde0756a5414e0d127175f77fed7c1eeb4c706bf
maltokyo <maltokyo@gmail.com>
2020-02-14 10:16:36 +0100

    Integrate EPS (Electrum Personal Server) (#227)
    * Integrate EPS (Electrum Personal Server)
    
    After the ElectrumX integration in the last release, I thought that some people may prefer the Electrum Personal Server (EPS) approach, which can only serve specific pre-determined wallets (using XPUBS).  EPS cannot serve as a server for ad-hoc wallets with connections for many people, that ElectrumX delivers.  The upside pf EPS is that it is much lighter, and doesn't require txindex=1.
    
    I will integrate it with this pull request, but it might take a while, as I have some busy weeks ahead of me.
    
    * Update opt-add-electrum-ps.yml
    
    * Update opt-add-electrum-ps.yml
    
    * Update opt-add-electrum-ps.yml
    
    * Update opt-add-electrum-ps.yml
    
    * fix persistent data directory
    
    * Break up monolithic bitcoind var
    
    * fix bitcoind daemon name
    
    * Update opt-add-electrum-ps.yml
    
    * Update opt-add-electrum-ps.yml
    
    * Update opt-add-electrum-ps.yml
    
    * Fixed image name
    
    * fixed bitcoind rpc login credentials
    
    * Update opt-add-electrum-ps.yml
    
    * Add $BTCPAY_EPS_XPUB
    
    Add $BTCPAY_EPS_XPUB for Electrum Personal Server Integration
    
    * Update opt-add-electrum-ps.yml
    
    * Update opt-add-electrum-ps.yml
    
    * Update README.md
    
    * Update README.md
    
    * Update build.sh
    
    * Update opt-add-electrum-ps.yml
    
    * Update README.md
    
    Co-authored-by: peleion <github@peleion.com>

--- build.sh
+++ build.sh
@@ -32,6 +32,7 @@ docker run -v "$(pwd)/Generated:/app/Generated" \
            -e "BTCPAYGEN_LIGHTNING=$BTCPAYGEN_LIGHTNING" \
            -e "BTCPAYGEN_SUBNAME=$BTCPAYGEN_SUBNAME" \
            -e "BTCPAY_HOST_SSHAUTHORIZEDKEYS=$BTCPAY_HOST_SSHAUTHORIZEDKEYS" \
+           -e "EPS_XPUB=$EPS_XPUB" \
            --rm $BTCPAYGEN_DOCKER_IMAGE
 
 if [ "$BTCPAYGEN_REVERSEPROXY" == "nginx" ]; then
