fc2f0777d8b8e424157fe8b60d82ee189cb84ecf
Anthony Potdevin <31413433+apotdevin@users.noreply.github.com>
2020-04-20 23:39:18 +0200

    chore: bump version (#1)
    * chore: version bump
    
    * chore: update repo

--- contrib/build-all-images.sh
+++ contrib/build-all-images.sh
@@ -640,14 +640,14 @@ cd - && cd ..
 
 
 # Build thunderhub
-# https://raw.githubusercontent.com/apotdevin/thunderhub/v0.3.6/Dockerfile
+# https://raw.githubusercontent.com/apotdevin/thunderhub/v0.3.8/Dockerfile
 DOCKERFILE="Dockerfile"
-echo "Building apotdevin/thunderhub:v0.3.6"
+echo "Building apotdevin/thunderhub:v0.3.8"
 git clone https://github.com/apotdevin/thunderhub thunderhub
 cd thunderhub
-git checkout v0.3.6
+git checkout v0.3.8
 cd "$(dirname $DOCKERFILE)"
-docker build -f "$DOCKERFILE" -t "apotdevin/thunderhub:v0.3.6" .
+docker build -f "$DOCKERFILE" -t "apotdevin/thunderhub:v0.3.8" .
 cd - && cd ..
 
 
