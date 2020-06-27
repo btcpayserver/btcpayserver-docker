3fd903bedef1db3bfdf595f92b6063fccf95a6bb
AP <potdevin.anthony@gmail.com>
2020-06-27 21:56:20 +0200

    chore: bump version

--- contrib/build-all-images.sh
+++ contrib/build-all-images.sh
@@ -640,18 +640,18 @@ cd - && cd ..
 
 
 # Build thunderhub
-# https://raw.githubusercontent.com/apotdevin/thunderhub/v0.7.9/Dockerfile
+# https://raw.githubusercontent.com/apotdevin/thunderhub/v0.8.5/Dockerfile
 DOCKERFILE="Dockerfile"
-# https://raw.githubusercontent.com/apotdevin/thunderhub/v0.7.9/arm32v7.Dockerfile
+# https://raw.githubusercontent.com/apotdevin/thunderhub/v0.8.5/arm32v7.Dockerfile
 [[ "$(uname -m)" == "armv7l" ]] && DOCKERFILE="arm32v7.Dockerfile"
-# https://raw.githubusercontent.com/apotdevin/thunderhub/v0.7.9/arm64v8.Dockerfile
+# https://raw.githubusercontent.com/apotdevin/thunderhub/v0.8.5/arm64v8.Dockerfile
 [[ "$(uname -m)" == "aarch64" ]] && DOCKERFILE="arm64v8.Dockerfile"
-echo "Building apotdevin/thunderhub:v0.7.9"
+echo "Building apotdevin/thunderhub:v0.8.5"
 git clone https://github.com/apotdevin/thunderhub thunderhub
 cd thunderhub
-git checkout v0.7.9
+git checkout v0.8.5
 cd "$(dirname $DOCKERFILE)"
-docker build -f "$DOCKERFILE" -t "apotdevin/thunderhub:v0.7.9" .
+docker build -f "$DOCKERFILE" -t "apotdevin/thunderhub:v0.8.5" .
 cd - && cd ..
 
 
