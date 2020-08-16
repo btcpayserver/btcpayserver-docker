c0805e4949fe5e925fbaf47aa46d8a91f659cbe7
nicolas.dorier <nicolas.dorier@gmail.com>
2020-08-16 23:35:59 +0900

    bump

--- contrib/build-all-images.sh
+++ contrib/build-all-images.sh
@@ -242,18 +242,18 @@ cd - && cd ..
 
 
 # Build btcpayserver
-# https://raw.githubusercontent.com/btcpayserver/btcpayserver/v1.0.5.4/amd64.Dockerfile
+# https://raw.githubusercontent.com/btcpayserver/btcpayserver/v1.0.5.5/amd64.Dockerfile
 DOCKERFILE="amd64.Dockerfile"
-# https://raw.githubusercontent.com/btcpayserver/btcpayserver/v1.0.5.4/arm32v7.Dockerfile
+# https://raw.githubusercontent.com/btcpayserver/btcpayserver/v1.0.5.5/arm32v7.Dockerfile
 [[ "$(uname -m)" == "armv7l" ]] && DOCKERFILE="arm32v7.Dockerfile"
-# https://raw.githubusercontent.com/btcpayserver/btcpayserver/v1.0.5.4/arm64v8.Dockerfile
+# https://raw.githubusercontent.com/btcpayserver/btcpayserver/v1.0.5.5/arm64v8.Dockerfile
 [[ "$(uname -m)" == "aarch64" ]] && DOCKERFILE="arm64v8.Dockerfile"
-echo "Building btcpayserver/btcpayserver:1.0.5.4$<BTCPAY_BUILD_CONFIGURATION>"
+echo "Building btcpayserver/btcpayserver:1.0.5.5$<BTCPAY_BUILD_CONFIGURATION>?"
 git clone https://github.com/btcpayserver/btcpayserver btcpayserver
 cd btcpayserver
-git checkout v1.0.5.4
+git checkout v1.0.5.5
 cd "$(dirname $DOCKERFILE)"
-docker build -f "$DOCKERFILE" -t "btcpayserver/btcpayserver:1.0.5.4$<BTCPAY_BUILD_CONFIGURATION>" .
+docker build -f "$DOCKERFILE" -t "btcpayserver/btcpayserver:1.0.5.5$<BTCPAY_BUILD_CONFIGURATION>?" .
 cd - && cd ..
 
 
