74180f7b94662eedb1aeb072c4e511b7d98d8603
rockstardev <5191402+rockstardev@users.noreply.github.com>
2021-04-22 23:03:46 -0500

    Adding tool to recreate tls certificate in LND container

---
+++ Tools/lnd_delete_tls.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+read -p "This script will delete LND's TLS certificate, so that's it's recreated on restart. Please keep in mind that you'll need to update external connections to LND that depend on TLS cert. Type 'yes' to confirm you want to proceed`echo $'\n> '`" yn
+if [ $yn != "yes" ]; then
+	exit 0
+fi
+
+docker exec btcpayserver_lnd_bitcoin rm -rf /root/.lnd/tls.cert
+docker exec btcpayserver_lnd_bitcoin rm -rf /root/.lnd/tls.key
+
+. btcpay-setup.sh -i
+
+echo "LND TLS certificate recreated"
