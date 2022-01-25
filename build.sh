b3804186de086734d2e163144b974528891170fd
Honza Pobořil <honza@poboril.cz>
2022-01-25 02:33:40 +0100

    Removed not needed files copying.

--- build.sh
+++ build.sh
@@ -41,9 +41,3 @@ fi
 
 [[ -f "Generated/pull-images.sh" ]] && chmod +x Generated/pull-images.sh
 [[ -f "Generated/save-images.sh" ]] && chmod +x Generated/save-images.sh
-
-if [ "$BTCPAYGEN_REVERSEPROXY" == "traefik" ]; then
-    cp Traefik/traefik.toml Generated/traefik.toml
-    :> Generated/acme.json
-    chmod 600 Generated/acme.json
-fi
