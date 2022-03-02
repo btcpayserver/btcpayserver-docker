772358fdbf1fa3373f225c098a8c724546f65a3d
iam1412 <9153228+iam1412@users.noreply.github.com>
2022-03-02 17:59:25 +0700

    Fixed git -c command for CentOS Linux
    Fixed git -C to git -c for CentOS Linux Command

--- btcpay-setup.sh
+++ btcpay-setup.sh
@@ -38,14 +38,28 @@ else
 fi
 
 # Verify we are in right folder. If we are not, let's go in the parent folder of the current docker-compose.
-if ! git -C . rev-parse &> /dev/null || [ ! -d "Generated" ]; then
-    if [[ ! -z $BTCPAY_DOCKER_COMPOSE ]]; then
-        cd $(dirname $BTCPAY_DOCKER_COMPOSE)
-        cd ..
+# Check for CentOS git command git -c
+if [ "$(. /etc/os-release; echo $NAME)" = "CentOS Linux" ]; then
+    if ! git -c . rev-parse &> /dev/null || [ ! -d "Generated" ]; then
+        if [[ ! -z $BTCPAY_DOCKER_COMPOSE ]]; then
+            cd $(dirname $BTCPAY_DOCKER_COMPOSE)
+            cd ..
+        fi
+        if ! git -c . rev-parse || [[ ! -d "Generated" ]]; then
+            echo "You must run this script inside the git repository of btcpayserver-docker"
+            return
+        fi
     fi
-    if ! git -C . rev-parse || [[ ! -d "Generated" ]]; then
-        echo "You must run this script inside the git repository of btcpayserver-docker"
-        return
+else
+    if ! git -C . rev-parse &> /dev/null || [ ! -d "Generated" ]; then
+        if [[ ! -z $BTCPAY_DOCKER_COMPOSE ]]; then
+            cd $(dirname $BTCPAY_DOCKER_COMPOSE)
+            cd ..
+        fi
+        if ! git -C . rev-parse || [[ ! -d "Generated" ]]; then
+            echo "You must run this script inside the git repository of btcpayserver-docker"
+            return
+        fi
     fi
 fi
 
