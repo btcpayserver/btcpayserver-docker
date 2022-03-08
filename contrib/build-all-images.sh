207c38ceabc040c691d00c018d0e2359c2f9c966
Kukks <evilkukka@gmail.com>
2022-03-08 08:31:35 +0100

    Bump liquid

--- contrib/build-all-images.sh
+++ contrib/build-all-images.sh
@@ -374,18 +374,18 @@ cd - && cd ..
 
 
 # Build elements
-# https://raw.githubusercontent.com/btcpayserver/dockerfile-deps/Elements/0.21.0.1/Elements/0.21.0.1/linuxamd64.Dockerfile
-DOCKERFILE="Elements/0.21.0.1/linuxamd64.Dockerfile"
-# https://raw.githubusercontent.com/btcpayserver/dockerfile-deps/Elements/0.21.0.1/Elements/0.21.0.1/linuxarm32v7.Dockerfile
-[[ "$(uname -m)" == "armv7l" ]] && DOCKERFILE="Elements/0.21.0.1/linuxarm32v7.Dockerfile"
-# https://raw.githubusercontent.com/btcpayserver/dockerfile-deps/Elements/0.21.0.1/Elements/0.21.0.1/linuxarm64v8.Dockerfile
-[[ "$(uname -m)" == "aarch64" ]] && DOCKERFILE="Elements/0.21.0.1/linuxarm64v8.Dockerfile"
-echo "Building btcpayserver/elements:0.21.0.1"
+# https://raw.githubusercontent.com/btcpayserver/dockerfile-deps/Elements/0.21.0.2/Elements/0.21.0.2/linuxamd64.Dockerfile
+DOCKERFILE="Elements/0.21.0.2/linuxamd64.Dockerfile"
+# https://raw.githubusercontent.com/btcpayserver/dockerfile-deps/Elements/0.21.0.2/Elements/0.21.0.2/linuxarm32v7.Dockerfile
+[[ "$(uname -m)" == "armv7l" ]] && DOCKERFILE="Elements/0.21.0.2/linuxarm32v7.Dockerfile"
+# https://raw.githubusercontent.com/btcpayserver/dockerfile-deps/Elements/0.21.0.2/Elements/0.21.0.2/linuxarm64v8.Dockerfile
+[[ "$(uname -m)" == "aarch64" ]] && DOCKERFILE="Elements/0.21.0.2/linuxarm64v8.Dockerfile"
+echo "Building btcpayserver/elements:0.21.0.2"
 git clone https://github.com/btcpayserver/dockerfile-deps elements
 cd elements
-git checkout Elements/0.21.0.1
+git checkout Elements/0.21.0.2
 cd "$(dirname $DOCKERFILE)"
-docker build -f "$DOCKERFILE" -t "btcpayserver/elements:0.21.0.1" .
+docker build -f "$DOCKERFILE" -t "btcpayserver/elements:0.21.0.2" .
 cd - && cd ..
 
 
