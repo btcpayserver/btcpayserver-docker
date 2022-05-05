fc87f98ca8789e3551bb127af65ac5607fa4d37e
Dennis Reimann <mail@dennisreimann.de>
2022-05-05 11:39:33 +0200

    Add basic backup and restore scripts

---
+++ btcpay-backup.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Please be aware of these important issues:
+#
+# - Old channel state is toxic and you can loose all your funds, if you or someone
+#   else closes a channel based on the backup with old state - and the state changes
+#   often! If you publish an old state (say from yesterday's backup) on chain, you
+#   WILL LOSE ALL YOUR FUNDS IN A CHANNEL, because the counterparty will publish a
+#   revocation key!
+
+if [ "$(id -u)" != "0" ]; then
+  echo "This script must be run as root."
+  echo "Use the command 'sudo su -' (include the trailing hypen) and try again"
+  exit 1
+fi
+
+# preparation
+docker_dir=/var/lib/docker
+dbdump_name=postgres.sql
+btcpay_dir="$BTCPAY_BASE_DIRECTORY/btcpayserver-docker"
+backup_dir="$docker_dir/volumes/backup_datadir/_data"
+dbdump_path="$docker_dir/$dbdump_name"
+backup_path="$backup_dir/backup.tar.gz"
+
+# ensure backup dir exists
+if [ ! -d "$backup_dir" ]; then
+  docker volume create backup_datadir
+fi
+
+cd $btcpay_dir
+. helpers.sh
+
+echo "Stopping BTCPay Server …"
+btcpay_down
+
+echo "Dumping database …"
+btcpay_dump_db $dbdump_path
+
+echo "Backing up files …"
+cd $docker_dir
+tar \
+  --exclude="volumes/backup_datadir" \
+  --exclude="volumes/generated_postgres_datadir" \
+  --exclude="volumes/generated_bitcoin_datadir" \
+  --exclude="volumes/generated_litecoin_datadir" \
+  --exclude="**/logs/*" \
+  -cvzf $backup_path $dbdump_name volumes/generated_*
+cd -
+
+echo "Restarting BTCPay Server …"
+btcpay_up
+
+echo "Cleaning up …"
+rm $dbdump_path
+
+echo "Backup done."
---
+++ btcpay-restore.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ "$(id -u)" != "0" ]; then
+  echo "This script must be run as root."
+  echo "Use the command 'sudo su -' (include the trailing hypen) and try again"
+  exit 1
+fi
+
+backup_file=$1
+if [ -z "$backup_file" ]; then
+  echo "Usage: btcpay-restore.sh /path/to/backup.tar.gz"
+  exit
+fi
+
+if [ ! -f "$backup_file" ]; then
+  echo "$backup_file does not exist."
+  exit
+fi
+
+volumes_dir=/var/lib/docker/volumes
+restore_dir="$volumes_dir/backup_datadir/_data/restore"
+
+mkdir -p $restore_dir
+tar -xvf $backup_file -C $restore_dir
