152065ce8630a2688d9261e9a6c832e8c1a4bbfe
nicolas.dorier <nicolas.dorier@gmail.com>
2025-11-12 00:20:56 +0900

    fix compose-bin in doc

--- docker-compose-generator/src/DockerComposeDefinition.cs
+++ docker-compose-generator/src/DockerComposeDefinition.cs
@@ -175,7 +175,7 @@ namespace DockerGenerator
 			PostProcess(output);
 
 			var dockerImages = ((YamlMappingNode)output["services"]).Children.Select(kv => kv.Value["image"].ToString()).ToList();
-			dockerImages.Add("docker/compose-bin:v2.23.3");
+			dockerImages.Add("docker/compose-bin:v2.40.3");
 			dockerImages.Add("btcpayserver/docker-compose-generator:latest");
 			StringBuilder pullImageSh = new StringBuilder();
 			pullImageSh.Append($"#!/bin/bash\n\n");
