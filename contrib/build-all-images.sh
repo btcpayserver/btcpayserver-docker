162b127073b3ba675c59398ab92a21913ad5a296
Nicolas Dorier <nicolas.dorier@gmail.com>
2026-09-03 13:50:44 +0900

    Bump NBXplorer to 2.6.12

--- contrib/build-all-images.sh
+++ contrib/build-all-images.sh
@@ -372,18 +372,18 @@ cd - && cd ..
 
 
 # Build nbxplorer
-# https://raw.githubusercontent.com/dgarage/nbxplorer/v2.6.11/Dockerfile
+# https://raw.githubusercontent.com/dgarage/nbxplorer/v2.6.12/Dockerfile
 DOCKERFILE="Dockerfile"
-# https://raw.githubusercontent.com/dgarage/nbxplorer/v2.6.11/Dockerfile
+# https://raw.githubusercontent.com/dgarage/nbxplorer/v2.6.12/Dockerfile
 [[ "$(uname -m)" == "armv7l" ]] && DOCKERFILE="Dockerfile"
-# https://raw.githubusercontent.com/dgarage/nbxplorer/v2.6.11/Dockerfile
+# https://raw.githubusercontent.com/dgarage/nbxplorer/v2.6.12/Dockerfile
 [[ "$(uname -m)" == "aarch64" ]] && DOCKERFILE="Dockerfile"
-echo "Building nicolasdorier/nbxplorer:2.6.11"
+echo "Building nicolasdorier/nbxplorer:2.6.12"
 git clone https://github.com/dgarage/nbxplorer nbxplorer
 cd nbxplorer
-git checkout v2.6.11
+git checkout v2.6.12
 cd "$(dirname $DOCKERFILE)"
-docker build -f "$DOCKERFILE" -t "nicolasdorier/nbxplorer:2.6.11" .
+docker build -f "$DOCKERFILE" -t "nicolasdorier/nbxplorer:2.6.12" .
 cd - && cd ..
 
 
