651ae54b2e7ab9ce56430adf4a7a98735533de77
Nicolas Dorier <nicolas.dorier@gmail.com>
2026-09-18 17:41:54 +0900

    Bump Litecoin Core to 0.21.5.8

--- contrib/build-all-images.sh
+++ contrib/build-all-images.sh
@@ -232,18 +232,18 @@ cd - && cd ..
 
 
 # Build litecoin
-# https://raw.githubusercontent.com/btcpayserver/dockerfile-deps/Litecoin/0.21.5.6/Litecoin/0.21.5.6/linuxamd64.Dockerfile
-DOCKERFILE="Litecoin/0.21.5.6/linuxamd64.Dockerfile"
-# https://raw.githubusercontent.com/btcpayserver/dockerfile-deps/Litecoin/0.21.5.6/Litecoin/0.21.5.6/linuxarm32v7.Dockerfile
-[[ "$(uname -m)" == "armv7l" ]] && DOCKERFILE="Litecoin/0.21.5.6/linuxarm32v7.Dockerfile"
-# https://raw.githubusercontent.com/btcpayserver/dockerfile-deps/Litecoin/0.21.5.6/Litecoin/0.21.5.6/linuxarm64v8.Dockerfile
-[[ "$(uname -m)" == "aarch64" ]] && DOCKERFILE="Litecoin/0.21.5.6/linuxarm64v8.Dockerfile"
-echo "Building btcpayserver/litecoin:0.21.5.6"
+# https://raw.githubusercontent.com/btcpayserver/dockerfile-deps/Litecoin/0.21.5.8/Litecoin/0.21.5.8/linuxamd64.Dockerfile
+DOCKERFILE="Litecoin/0.21.5.8/linuxamd64.Dockerfile"
+# https://raw.githubusercontent.com/btcpayserver/dockerfile-deps/Litecoin/0.21.5.8/Litecoin/0.21.5.8/linuxarm32v7.Dockerfile
+[[ "$(uname -m)" == "armv7l" ]] && DOCKERFILE="Litecoin/0.21.5.8/linuxarm32v7.Dockerfile"
+# https://raw.githubusercontent.com/btcpayserver/dockerfile-deps/Litecoin/0.21.5.8/Litecoin/0.21.5.8/linuxarm64v8.Dockerfile
+[[ "$(uname -m)" == "aarch64" ]] && DOCKERFILE="Litecoin/0.21.5.8/linuxarm64v8.Dockerfile"
+echo "Building btcpayserver/litecoin:0.21.5.8"
 git clone https://github.com/btcpayserver/dockerfile-deps litecoin
 cd litecoin
-git checkout Litecoin/0.21.5.6
+git checkout Litecoin/0.21.5.8
 cd "$(dirname $DOCKERFILE)"
-docker build -f "$DOCKERFILE" -t "btcpayserver/litecoin:0.21.5.6" .
+docker build -f "$DOCKERFILE" -t "btcpayserver/litecoin:0.21.5.8" .
 cd - && cd ..
 
 
